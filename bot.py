0d262c158f66a56fbdb70be764690a1247ac7a1b
Nick <nickknapton12@gmail.com>
2021-06-08 22:04:40 -0700

    Re added events

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 import time
 import asyncio
 
-print(discord.__version__)
-
+#Reads token from a token.txt file placed in the .src directory. Token should be the token
+#given when discord bot is registered and should remane secret at all times!
 def read_token():
     with open("./src/token.txt","r") as f:
         lines = f.readlines()
@@ -11,11 +11,14 @@ def read_token():
 
 token = read_token()
 
+# Specifys intent and gives access to all discord members in a server
 intents = discord.Intents.default()
 intents.members = True
 
+# Setup for Client
 client = discord.Client(intents = intents,activity=discord.Activity(type=discord.ActivityType.watching, name="out for you!"))
 
+# OnReady Definition, runs every time bot is started up and ready
 @client.event
 async def on_ready():
     guild = client.get_guild(802294668913410129)
@@ -26,9 +29,9 @@ async def on_ready():
         else:
             print(f"Bot acount: {member.name}")
 
-
+# OnMessage Definition, runs everytime bot detects a message
 @client.event
-async def on_message(message):
+async def on_messages(message):
     if message.author != client.user:
         #Should send a list of all current reminders to user based on their subscritptions
         #if in a DM or reminders related to class if in a class related massage
@@ -38,4 +41,5 @@ async def on_message(message):
             print()
         
 
+# Starts Bot
 client.run(token)
