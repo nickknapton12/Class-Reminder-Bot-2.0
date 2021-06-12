98c459c1071ef8aaccbeef18ad1cfdddd2ac3de1
Nick <nickknapton12@gmail.com>
2021-06-11 22:07:16 -0700

    Add basic functions

--- src/reminder_functions.py
+++ src/reminder_functions.py
@@ -5,12 +5,20 @@ import discord
 # when looping through users, just search for each reminder the user is subed
 # to making only one loop necassary.
 def send_morning_reminders(client):
-    print()
+    print("")
 
 # Sends a welcome message to a new user
 def send_welcome_message(client, user):
-    print()
+    print("")
 
 # Sends a "Back Up" to existing users when the bot comes back online.
 def send_back_up_message(client, user):
-    print()
+    print("")
+
+# Sends a DM to User with personilized reminders.
+def send_user_current_reminders(client, user):
+    print("")
+
+# Sends a message of all the current reminders specific to the channel.
+def send_channel_reminders(client, channel):
+    print("")
