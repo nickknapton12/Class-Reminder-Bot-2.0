b128a9d7dffe3ce8f8d571ae18b846c99c91dfd1
Nick <nickknapton12@gmail.com>
2021-06-11 21:36:38 -0700

    definitions for reminder functions

---
+++ src/reminder_functions.py
@@ -0,0 +1,16 @@
+import discord
+
+# Should query db for all reminders and users, cache them, then go through
+# all users and send their reminders. Could store reminders in hashmap and
+# when looping through users, just search for each reminder the user is subed
+# to making only one loop necassary.
+def send_morning_reminders(client):
+    print()
+
+# Sends a welcome message to a new user
+def send_welcome_message(client, user):
+    print()
+
+# Sends a "Back Up" to existing users when the bot comes back online.
+def send_back_up_message(client, user):
+    print()
